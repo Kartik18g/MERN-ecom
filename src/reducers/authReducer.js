ba3910a3166dcb7e924c2e70053da54e49d00baa
Kartik18g <gupta.kartik18kg@gmail.com>
2021-11-20 19:06:56 +0530

    redux setup

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    token: null
+}
+
+export default function authReducer(state = initialState, action) {
+    const { type, payload } = action
+
+    switch (type) {
+        default:
+            return state
+    }
+
+}
